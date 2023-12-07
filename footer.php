7e850d0a8751ce6a46b383c8b4d849f24a013fc0
Austin Crane <austincrane@gmail.com>
2023-12-07 09:31:26 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <div id="bw-tagline">trust+<strong>strategy</strong>+integrity+<strong>planning</strong>+insight+<strong>experience</strong> <span class="bw-tagline-font1">...it all <strong>adds</strong> up.</span></div>
 
 <div id="bw-footer">
-<div id="bw-footer2">2815 Coliseum Centre Drive | Suite 200 | Charlotte, NC 28217<br/><a href="<?php bloginfo('url'); ?>/sitemap/">sitemap</a></div>
+<div id="bw-footer2">3800 Arco Corporate Drive | Suite 500 | Charlotte, NC 28273<br/><a href="<?php bloginfo('url'); ?>/sitemap/">sitemap</a></div>
 
 <div id="bw-footer3"><a href="http://www.upstreamacademy.com/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/upstream-academy-member.jpg" alt="Upstream Academy Member" border="0" align="right"></a><br />&nbsp;Member of Upstream Academy Network, a premiere community of CPA firms committed to excellence through continuous improvement.
 </div>
